9360c303e7fec49dc4347ce05ed203acc9ef5021
Paddy Keane <Paddy.Keane@akqa.com>
2022-10-14 15:35:57 +0100

    adding netlify adapter

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,14 +7,16 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact(), partytown(),
-    tailwind({
-      config: {
-        path: "../../packages/ui/tailwind.config.cjs"
-      }
-    })]
+  integrations: [preact(), partytown(), tailwind({
+    config: {
+      path: "../../packages/ui/tailwind.config.cjs"
+    }
+  })],
+  output: "server",
+  adapter: netlify()
 });
